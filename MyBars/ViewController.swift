812af685239c543b93b1e147642e7af882de3974
sasha.cassis <alexander.dorogoff@icloud.com>
2022-02-18 19:04:17 +0300

    Initial Commit

---
+++ MyBars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBars
+//
+//  Created by Alexandr Dorogov on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
